c57026c93279e51300fdd6c2ee8912e6f66f54a0
Kevin Fries <kevinsctfries@gmail.com>
2025-10-27 18:24:26 -0400

    feat: add copy btn to shortened url

--- frontend/src/components/QRCodeGenerator/QRCodeGenerator.jsx
+++ frontend/src/components/QRCodeGenerator/QRCodeGenerator.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import { QRCodeSVG } from "qrcode.react";
 import "./QRCodeGenerator.scss";
 
 export default function QRCodeGenerator({ url }) {
+  const [copied, setCopied] = useState(false);
+
   if (!url) return null;
+
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1000);
+      })
+      .catch(err => console.error("Failed to copy", err));
+  };
 
   return (
     <div className="qr-container">
-      <p>
-        Shortened URL: <a href={url}>{url}</a>
-      </p>
+      <div className="url-copy-wrapper">
+        <a href={url}>{url}</a>
+        <button onClick={handleCopy} className="copy-btn" aria-label="Copy URL">
+          <img
+            src="/copy.svg"
+            alt="Copy icon"
+            className={copied ? "copied" : ""}
+          />
+        </button>
+      </div>
       <QRCodeSVG value={url} />
     </div>
   );
